1033a84b54d9e478b72da38274c50c4967440fc9
Chris Loughnane <loughnane.c@gmail.com>
2016-01-13 23:28:36 -0500

    added phase gate rects, though no labels yet

--- process/main.js
+++ process/main.js
@@ -38,18 +38,30 @@ window.onload=function(){
     ////////////SIZING////////
 
     //overall parameters
-    var width = 1400;
-    var height = 800;
+    var width = 1200;
+    var height = 500;
     var margin = 25;
+
+    var cell_width = 0.7
+    var cell_height = 0.9
+
+    var matrix_offset_y = 0.5
+    var matrix_offset_x = 1
+
+    var general_clearance = 0.05
+    var heading_size = 0.1
+
+    var num_gates =3
+    var gate_width = (1-cell_width-general_clearance*2)/num_gates
 
     //scales
     var x = d3.scale.linear()
-      .domain([0,phase_count])
-      .range([0,width*0.9]);
+      .domain([0,phase_count+matrix_offset_x])
+      .range([0,width]);
 
     var y = d3.scale.linear()
-      .domain([0,phase_count])
-      .range([0,height*0.9]);
+      .domain([0,discipline_count+matrix_offset_y])
+      .range([0,height-(num_gates+1)*x(gate_width)]);
 
 
     ///////////////SELECTIONS
@@ -68,21 +80,22 @@ window.onload=function(){
     .enter()
     .append("g")
     .attr("class","heading")
-    .attr("transform",function(d,i){return "translate(" + x(i*.9+1.3) + "," + y(0.5)+")"})
+    .attr("transform",function(d,i){
+      return "translate(" + x(i+matrix_offset_x+general_clearance) + "," + y(matrix_offset_y-general_clearance)+")"})
     .append('text')
-    .text(function(d){return phases[d]})
+    .attr('font-size',x(heading_size))
+    .text(function(d){return phases[d].title});
 
-
-
-    //map the rows to the top level objects in the array
+    //rows
     var row = svg.selectAll(".row")
       .data(matrix)
       .enter()
       .append("g")
       .attr("class", "row")
       .attr("id",function(d,i){ return discipline_keys[i]})
-      .attr("transform", function(d, i) { return "translate(0," + y(i+0.6) + ")"; })
+      .attr("transform", function(d, i) { return "translate(0," + y(i+matrix_offset_y) + ")"; })
       .each(row);
+
 
     //for each row above, iterate through it's objects and draw rectangles
     function row(row) {
@@ -92,14 +105,13 @@ window.onload=function(){
             .data(row)
             .enter()
             .append("g")
-            .attr("transform",function(d,i){return "translate(" + x(d.x*.9+1) +",0)"});
+            .attr("transform",function(d,i){return "translate(" + x(d.x+matrix_offset_x) +",0)"});
 
             //append the rectange to the selection
             cell.append("rect")
             .attr("class", "cell")    
-            .attr("width", x(0.8))
-            .attr("height", y(0.9))
-
+            .attr("width", x(cell_width))
+            .attr("height", y(cell_height))
             .on("mouseover", mouseover)
             .on("mouseout", mouseout);
 
@@ -112,17 +124,46 @@ window.onload=function(){
     function tasks(tasks){
       task_list= blocks.filter(function(d){
         return d.phase===phase_keys[tasks.x] && d.discipline===discipline_keys[tasks.y]})[0].tasks
-    d3.select(this).selectAll(".text")
-      .data(task_list)
-      .enter()
-      .append('text')
-      .attr("x",x(0.05))
-      .attr("y",function(d,i){return y(0.07)*i+y(0.1)})
-      .attr("font-size",y(0.05))
-      .text(function(d){return "- " + d});
+      d3.select(this).selectAll(".text")
+        .data(task_list)
+        .enter()
+        .append('text')
+        .attr("x",x(0.05))
+        .attr("y",function(d,i){return y(0.07)*i+y(0.1)})
+        .attr("font-size",y(0.05))
+        .text(function(d){return "- " + d});
 
     }
+    //gates
 
+    var gates = svg.selectAll(".gate")
+      .data(phase_keys)
+      .enter()
+      .append("g")
+      .attr("class","gate")
+      .attr("transform",function(d,i){
+        console.log(phases[d].gates,i)
+        return "translate(" + x(i+matrix_offset_x) +",0)"})
+      .each(gates)
+
+    function gates(gates){
+      gates_list = phases[gates].gates
+      gate_count = gates_list.length
+      console.log(gates_list)
+      d3.select(this).selectAll("gate")
+      .data(gates_list)
+      .enter()
+      .append("rect")
+      .attr("class","gate")
+      .attr("width",x(gate_width))
+      .attr("height",function(d,i){
+        
+        return height-y(matrix_offset_y)-x((gate_count-i)*(gate_width+general_clearance));})
+      .attr("x",function(d,i){return x(cell_width + general_clearance) + x(i*(gate_width+general_clearance))})
+      .attr("y",y(matrix_offset_y));
+
+
+    }
 
     //////////INTERACTIVITY
 
